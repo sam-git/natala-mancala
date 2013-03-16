07d2d7cd9dd23128562e128264bcb2beeb735c46
Samuel Grace <smrgrace@gmail.com>
2013-03-17 10:54:30 +1300

    add .java files

---
+++ se701-a1/src/HelloA1.java
@@ -0,0 +1,13 @@
+
+public class HelloA1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Assignment 1");
+
+	}
+
+}
---
+++ se701-a2/src/HelloA2.java
@@ -0,0 +1,13 @@
+
+public class HelloA2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Assignment 2");
+
+	}
+
+}
